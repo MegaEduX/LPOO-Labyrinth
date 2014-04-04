20837ba0eb50300195d0d169e03dc0939f7c3f17
Joao <netforum@gmail.com>
2014-04-04 16:21:53 +0100

    aguia..

--- src/pt/up/fe/lpoo/logic/piece/itemizable/Eagle.java
+++ src/pt/up/fe/lpoo/logic/piece/itemizable/Eagle.java
@@ -8,18 +8,10 @@ package pt.up.fe.lpoo.logic.piece.itemizable;
 
 import pt.up.fe.lpoo.logic.Board;
 import pt.up.fe.lpoo.logic.Coordinate;
-import pt.up.fe.lpoo.logic.piece.Piece;
 
 public class Eagle extends ItemizablePiece {
     public Boolean _onGround = false;
     protected Coordinate FirstPos;
-
-    public Eagle(Boolean onGround, Coordinate pos) {
-        super();
-        _onGround = onGround;
-        FirstPos = new Coordinate(pos.x, pos.y);
-        _position = pos;
-    }
 
     public void setOnGround() {
         _onGround = true;
@@ -39,31 +31,6 @@ public class Eagle extends ItemizablePiece {
         throw new Exception("This can't be called, bro!");
     }
 
-    public Boolean moveEagle(Board.Direction dir) throws Exception {
-        Piece nextObj;
-        Coordinate crdDiff = new Coordinate(0, 0);
-
-        try {
-            nextObj = _moveSharedCode(dir, crdDiff);
-        } catch (Exception exc) {
-            throw exc;
-        }
-
-        Integer x = crdDiff.x, y = crdDiff.y;
-
-
-        if (((ItemizablePiece) nextObj).getHasItem()) {
-            setHasItem(true);
-
-            ((ItemizablePiece) nextObj).setHasItem(false);
-        }
-
-        nextObj.setCoordinate(_position);
-
-        _position = new Coordinate(_position.x + x, _position.y + y);
-
-        return true;
-    }
 
     public Boolean move(Coordinate c1) throws Exception {
         // Sword on the same line
@@ -72,44 +39,38 @@ public class Eagle extends ItemizablePiece {
 
             if (_position.y == c1.y) {
                 if (c1.x < _position.x)
-                    return moveEagle(Board.Direction.LEFT);
+                    return super.move(Board.Direction.LEFT);
                 if (c1.x > _position.x)
-                    return moveEagle(Board.Direction.RIGHT);
+                    return super.move(Board.Direction.RIGHT);
             }
             // Sword on the same Column
             if (_position.x == c1.x) {
                 if (c1.y < _position.y)
-                    return moveEagle(Board.Direction.UP);
+                    return super.move(Board.Direction.UP);
                 if (c1.y > _position.y)
-                    return moveEagle(Board.Direction.DOWN);
+                    return super.move(Board.Direction.DOWN);
             }
             // Nor the same line or column
             if (_position.x < c1.x) {
                 if (_position.y < c1.y) {
-                    moveEagle(Board.Direction.DOWN);
-                    return moveEagle(Board.Direction.RIGHT);
+                    return (super.move(Board.Direction.DOWN) && super.move(Board.Direction.RIGHT));
                 }
                 if (_position.y > c1.y) {
-                    moveEagle(Board.Direction.UP);
-                    return moveEagle(Board.Direction.RIGHT);
+                    return (super.move(Board.Direction.UP) && super.move(Board.Direction.RIGHT));
                 }
 
             }
             if (_position.x > c1.x) {
                 if (_position.y < c1.y) {
-                    moveEagle(Board.Direction.DOWN);
-                    return moveEagle(Board.Direction.LEFT);
+                    return (super.move(Board.Direction.DOWN) && super.move(Board.Direction.LEFT));
                 }
                 if (_position.y > c1.y) {
-                    moveEagle(Board.Direction.UP);
-                    return moveEagle(Board.Direction.LEFT);
+                    return (super.move(Board.Direction.UP) && super.move(Board.Direction.LEFT));
                 }
-
             }
             // Found the Sword
             _item = true;
             // If nothing
-
             return false;
         } catch (Exception exc) {
             throw exc;
